2e70aed5599b37b4541b23ecc3b05eb585514572
AleksanderSuchodolski <129153686+AleksanderSuchodolski@users.noreply.github.com>
2023-12-01 00:39:13 +0100

    Modal huk

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,41 @@
-import { Component } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 const rootModal = document.querySelector('#root-modal');
 
-export class Modal extends Component {
-  componentDidMount = () => {
-    window.addEventListener('keydown', this.onEscapeCloseModal);
-  };
+export const Modal = ({ selectedPhoto: { largeImageURL, tags }, onClose }) => {
+  const onEscapeCloseModal = useCallback(
+    evt => {
+      if (evt.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  componentWillUnmount = () => {
-    window.removeEventListener('keydown', this.onEscapeCloseModal);
-  };
+  useEffect(() => {
+    const handleEscape = evt => onEscapeCloseModal(evt);
+    return () => {
+      window.addEventListener('keydown', handleEscape);
+    };
+  }, [onEscapeCloseModal]);
 
-  onEscapeCloseModal = evt => {
-    if (evt.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+  const onClickOverlay = useCallback(
+    evt => {
+      if (evt.target === evt.currentTarget) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  onClickOverlay = evt => {
-    if (evt.target === evt.currentTarget) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const {
-      selectedPhoto: { largeImageURL, tags },
-    } = this.props;
-
-    return createPortal(
-      <Overlay onClick={this.onClickOverlay}>
-        <ModalWindow>
-          <img src={largeImageURL} alt={tags} />
-        </ModalWindow>
-      </Overlay>,
-      rootModal
-    );
-  }
-}
+  return createPortal(
+    <Overlay onClick={onClickOverlay}>
+      <ModalWindow>
+        <img src={largeImageURL} alt={tags} />
+      </ModalWindow>
+    </Overlay>,
+    rootModal
+  );
+};
